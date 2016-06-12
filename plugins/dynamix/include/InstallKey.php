600930a263cea084cb77393b710b76ebd46f0a0c
bergware <ronald.vd.berg62@gmail.com>
2016-06-12 08:01:52 +0200

    Made copyright consistent and updated to 2016

--- plugins/dynamix/include/InstallKey.php
+++ plugins/dynamix/include/InstallKey.php
@@ -1,5 +1,5 @@
 <?PHP
-/* Copyright 2015, Lime Technology
+/* Copyright 2015-2016, Lime Technology
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 2,
